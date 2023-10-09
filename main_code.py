df033fcf066f02b90ac6b8c32d5b7155f9545893
Blayt05 <A00840268@tec.mx>
2023-10-08 20:12:15 -0600

    Avance Booz

--- main_code.py
+++ main_code.py
@@ -1,25 +1,21 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-#Esto lo hizo cyrce
-#"Nada"
-
-
-#afsafa
-#asfasfafa
-#swswsadwdawda
-
 plt.style.use('_mpl-gallery')
+
+def separador(lista_init):
+    lista_fin = []
+    for i in lista_init:
+        if i not in lista_fin:
+            lista_fin.append(i)
+    return lista_fin
+
+
 
 def publicaciones(data):
     list_years = data["Year"].tolist()
     list_y = []
-    for i in list_years:
-        if i not in list_y:
-            try:
-                list_y.append(int(i))
-            except:
-                list_y = list_y
+    
     list_count =[]
     for i in list_y:
         list_count.append(list_years.count(i))
@@ -34,14 +30,51 @@ def publicaciones(data):
 
     plt.show()
 
+def global_sales(data):
+    ventas = data["Global_Sales"].tolist()
+    company = data["Platform"].tolist()
+    list_plataform = separador(company)
 
+    list_sales = [0 for i in range(len(list_plataform))]
+    
+    for i in range(len(ventas)):
+        list_sales[list_plataform.index(company[i])] += ventas[i]
+    
+    print(list_plataform)
+    print(list_sales)
+    print (len(list_plataform))
+    print (len(list_sales))
+    plt.bar(list_plataform, list_sales)
+    plt.xticks(rotation=90)
+    plt.show()
+    
+    
+def salesNA_by_year(data):
+    ventas = data["NA_Sales"].tolist()
+    años = data["Year"].tolist()
+    list_years = separador(años)
+    list_sales = [0 for i in range(len(list_years))]
+
+    for i in range(len(ventas)):
+        list_sales[list_years.index(años[i])] += ventas[i]
+
+    print (list_years)
+    print (list_sales)
+    print (len(list_years))
+    print (len(list_sales))
+    plt.bar(list_years, list_sales)
+    plt.xticks(rotation=90)
+    plt.show()
+
+
+    
 
 
 def main():
-    data = pd.
     data = pd.read_csv("vgsales.csv")
-    publicaciones(data)
-
+    #publicaciones(data)
+    #global_sales(data)
+    salesNA_by_year(data)
 
 
 if __name__ == "__main__":
